d0e15f05032d514b5a2ab4a950f70d3baa576463
brandonabajelo <brandonabajelo@gmail.com>
2018-11-10 14:53:52 -0800

    Account stuff

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,23 +1,24 @@
 import React from "react";
-import { Image, Well } from "react-bootstrap";
-import placeholder from "../../images/placeholder.png";
+import { FormControl, ControlLabel, FormGroup, Button } from "react-bootstrap";
 
 const Account = () => {
   return (
     <div className="container">
       <div className="Account">
-        <Image
-          src={placeholder}
-          circle
-          style={{ margin: "10px 100px 10px 350px" }}
-        />
-        <Well style={{ width: "50%", margin: "auto" }} bsSize="small">
-          Username
-        </Well>
-        <br />
-        <Well style={{ width: "50%", margin: "auto" }} bsSize="small">
-          Email
-        </Well>
+        <h2 style={{ textAlign: "center" }}>Account</h2>
+        <div className="container">
+          <form style={{ marginTop: "10px" }}>
+            <FormGroup style={{ width: "50%", margin: "auto" }}>
+              <ControlLabel>Username</ControlLabel>
+              <FormControl type="text" placeholder="Username" />
+              <br />
+              <ControlLabel>Password</ControlLabel>
+              <FormControl type="text" placeholder="Password" />
+              <br />
+              <Button>Save</Button>
+            </FormGroup>
+          </form>
+        </div>
       </div>
     </div>
   );
