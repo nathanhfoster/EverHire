bfafa19d537ead4c3ba04e2549133d9be2888056
brandonabajelo <brandonabajelo@gmail.com>
2018-11-10 15:35:48 -0800

    Reset username, password

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 import { FormControl, ControlLabel, FormGroup, Button } from "react-bootstrap";
 
-const Account = () => {
-  return (
-    <div className="container">
-      <div className="Account">
-        <h2 style={{ textAlign: "center" }}>Account</h2>
-        <div className="container">
-          <form style={{ marginTop: "10px" }}>
-            <FormGroup style={{ width: "50%", margin: "auto" }}>
-              <ControlLabel>Username</ControlLabel>
-              <FormControl type="text" placeholder="Username" />
-              <br />
-              <ControlLabel>Password</ControlLabel>
-              <FormControl type="text" placeholder="Password" />
-              <br />
-              <Button>Save</Button>
-            </FormGroup>
-          </form>
+class Account extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handlePassword = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  handleUsername = e => {
+    this.setState({
+      username: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { username, password } = this.state;
+    console.log(username, password);
+  };
+
+  render() {
+    const { username, password } = this.state;
+    return (
+      <div className="container">
+        <div className="Account">
+          <h2 style={{ textAlign: "center", color: "white" }}>Account</h2>
+          <div className="container">
+            <form style={{ marginTop: "10px" }} onSubmit={this.handleSubmit}>
+              <FormGroup
+                style={{ width: "50%", margin: "auto" }}
+                submit={this.handleSubmit}
+              >
+                <ControlLabel style={{ color: "white" }}>Username</ControlLabel>
+                <FormControl
+                  value={username}
+                  type="text"
+                  placeholder="Username"
+                  onChange={this.handleUsername}
+                />
+                <br />
+                <ControlLabel style={{ color: "white" }}>Password</ControlLabel>
+                <FormControl
+                  value={password}
+                  type="password"
+                  placeholder="Password"
+                  onChange={this.handlePassword}
+                />
+                <br />
+                <Button type="submit">Save</Button>
+              </FormGroup>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Account;
