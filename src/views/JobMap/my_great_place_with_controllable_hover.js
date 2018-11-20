49b6cee6d9a8672f17b9d7d35745ead365ceb3fc
brandonabajelo <brandonabajelo@gmail.com>
2018-11-19 21:12:18 -0800

    Changed cards and map hoverthing

--- src/views/JobMap/my_great_place_with_controllable_hover.js
+++ src/views/JobMap/my_great_place_with_controllable_hover.js
@@ -1,57 +1,67 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import shouldPureComponentUpdate from 'react-pure-render/function';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import shouldPureComponentUpdate from "react-pure-render/function";
 
 import {
-  infoBoxStyle, greatPlaceStyle,
-  greatPlaceCircleStyle, greatPlaceCircleStyleHover,
-  greatPlaceStickStyle, greatPlaceStickStyleHover, greatPlaceStickStyleShadow} from './my_great_place_with_controllable_hover_styles.js';
+  infoBoxStyle,
+  greatPlaceStyle,
+  greatPlaceCircleStyle,
+  greatPlaceCircleStyleHover,
+  greatPlaceStickStyle,
+  greatPlaceStickStyleHover,
+  greatPlaceStickStyleShadow
+} from "./my_great_place_with_controllable_hover_styles.js";
 
 class MyGreatPlaceWithControllableHover extends Component {
   constructor(props) {
-    super(props)
-    this.state = {
-    }
+    super(props);
+    this.state = {};
   }
-  shouldComponentUpdate = shouldPureComponentUpdate
+  shouldComponentUpdate = shouldPureComponentUpdate;
 
   static propTypes = {
     $hover: PropTypes.bool,
     text: PropTypes.string,
     zIndex: PropTypes.number
-  }
+  };
 
-  static defaultProps = {}
+  static defaultProps = {};
 
   render() {
-    const {text, zIndex} = this.props;
+    const { text, zIndex, address, phone_number, title } = this.props;
 
     const style = {
       ...greatPlaceStyle,
       zIndex: this.props.$hover ? 1000 : zIndex
-    }
+    };
 
-    const circleStyle = this.props.$hover ? greatPlaceCircleStyleHover : greatPlaceCircleStyle;
-    const stickStyle = this.props.$hover ? greatPlaceStickStyleHover : greatPlaceStickStyle;
+    const circleStyle = this.props.$hover
+      ? greatPlaceCircleStyleHover
+      : greatPlaceCircleStyle;
+    const stickStyle = this.props.$hover
+      ? greatPlaceStickStyleHover
+      : greatPlaceStickStyle;
 
     return (
       <div style={style} className="center">
-        {!this.props.$hover ? [
-          <div style={greatPlaceStickStyleShadow} />,
-            <div style={circleStyle}>
-              {text}
-            </div>,
-          <div style={stickStyle} />
-         ]:[
-          <div style={infoBoxStyle}>HOVER BOX</div>,
-          <div style={greatPlaceStickStyleShadow} />,
-            <div style={circleStyle}>
-              {text}
-            </div>,
-          <div style={stickStyle} />
-        ]}
-    </div>
-    )
+        {!this.props.$hover
+          ? [
+              <div style={greatPlaceStickStyleShadow} />,
+              <div style={circleStyle}>{text}</div>,
+              <div style={stickStyle} />
+            ]
+          : [
+              <div style={infoBoxStyle}>
+                {`${title} \n`}
+                <div>{`${address}`}</div>
+                <div>{`${phone_number}`}</div>
+              </div>,
+              <div style={greatPlaceStickStyleShadow} />,
+              <div style={circleStyle}>{text}</div>,
+              <div style={stickStyle} />
+            ]}
+      </div>
+    );
   }
 }
-export default (MyGreatPlaceWithControllableHover)
+export default MyGreatPlaceWithControllableHover;
--- src/views/JobMap/my_great_place_with_controllable_hover_styles.js
+++ src/views/JobMap/my_great_place_with_controllable_hover_styles.js
@@ -1,37 +1,40 @@
-const K_CIRCLE_SIZE = 34
-const K_STICK_SIZE = 10
-const K_STICK_WIDTH = 3
+const K_CIRCLE_SIZE = 34;
+const K_STICK_SIZE = 10;
+const K_STICK_WIDTH = 3;
 
 const infoBoxStyle = {
-  margin: 'auto',
-  alignContent: 'center',
-  justifyContent: 'center',
-  textAlign: 'center',
-  height: '250%',
-  width: '500%',
+  display: "block",
+  margin: "auto",
+  padding: "6px",
+  alignContent: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  height: "150%",
+  width: "500%",
   bottom: K_CIRCLE_SIZE,
-  transform: 'scale(1.5)',
-  position: 'absolute',
-  willChange: 'transform',
-  backgroundRepeat: 'no-repeat',
-  transition: '-webkit-transform .425s cubic-bezier(0.485, 1.65, 0.545, 0.835)',
+  transform: "scale(1.5)",
+  position: "absolute",
+  willChange: "transform",
+  backgroundRepeat: "no-repeat",
+  transition: "-webkit-transform .425s cubic-bezier(0.485, 1.65, 0.545, 0.835)",
   zIndex: 6500,
-  backgroundColor: 'white',
-  boxShadow: "0px 1px 5px 0px var(--primaryBoxShadow), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)"
-}
+  backgroundColor: "white",
+  boxShadow:
+    "0px 1px 5px 0px var(--primaryBoxShadow), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)"
+};
 
 const greatPlaceStyle = {
   // initially any map object has left top corner at lat lng coordinates
   // it's on you to set object origin to 0,0 coordinates
-  position: 'absolute',
+  position: "absolute",
   width: K_CIRCLE_SIZE,
   height: K_CIRCLE_SIZE + K_STICK_SIZE,
   left: -K_CIRCLE_SIZE / 2,
   top: -(K_CIRCLE_SIZE + K_STICK_SIZE)
-}
+};
 
 const greatPlaceCircleStyle = {
-  position: 'absolute',
+  position: "absolute",
   left: 0,
   top: 0,
   width: K_CIRCLE_SIZE,
@@ -40,65 +43,68 @@ const greatPlaceCircleStyle = {
   borderRadius: K_CIRCLE_SIZE,
   color: "var(--primaryColor)",
   backgroundColor: "white",
-  textAlign: 'center',
-  boxShadow: "0px 1px 5px 0px var(--primaryBoxShadow), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)",
+  textAlign: "center",
+  boxShadow:
+    "0px 1px 5px 0px var(--primaryBoxShadow), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)",
   fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
   fontSize: 12,
-  fontWeight: 'bold',
+  fontWeight: "bold",
   padding: 0,
-  cursor: 'pointer',
-  boxShadow: '0 0 0 1px white',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center'
-}
+  cursor: "pointer",
+  boxShadow: "0 0 0 1px white",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+};
 
-const userLocation = {
-  
-}
+const userLocation = {};
 
 const greatPlaceCircleStyleHover = {
   ...greatPlaceCircleStyle,
-  border: '2px solid var(--quinaryColor)',
-  color: 'var(--quinaryColor)',
-  transform: 'scale(1.1)',
-  transition: '-webkit-transform 0.25s cubic-bezier(0.485, 1.65, 0.545, 0.835)',
-  transformOrigin: '15px 60px 0px',
-  zIndex: 6500,
-}
+  border: "2px solid var(--quinaryColor)",
+  color: "var(--quinaryColor)",
+  transform: "scale(1.1)",
+  transition: "-webkit-transform 0.25s cubic-bezier(0.485, 1.65, 0.545, 0.835)",
+  transformOrigin: "15px 60px 0px",
+  zIndex: 6500
+};
 
 const greatPlaceStickStyleShadow = {
-  position: 'absolute',
+  position: "absolute",
   left: K_CIRCLE_SIZE / 2 - K_STICK_WIDTH / 2,
   top: K_CIRCLE_SIZE,
   width: K_STICK_WIDTH,
   height: K_STICK_SIZE,
-  backgroundColor: 'var(--quinaryColor)',
-  boxShadow: '0 0 0 1px white'
-}
-
+  backgroundColor: "var(--quinaryColor)",
+  boxShadow: "0 0 0 1px white"
+};
 
 const greatPlaceStickStyle = {
-  position: 'absolute',
+  position: "absolute",
   left: K_CIRCLE_SIZE / 2 - K_STICK_WIDTH / 2,
   top: K_CIRCLE_SIZE,
   width: K_STICK_WIDTH,
   height: K_STICK_SIZE,
-  backgroundColor: 'var(--primaryColor)'
-}
+  backgroundColor: "var(--primaryColor)"
+};
 
 const greatPlaceStickStyleHover = {
   ...greatPlaceStickStyle,
-  backgroundColor: 'var(--secondaryColor)',
-  display: 'none',
-  position: 'absolute',
-  willChange: 'transform',
-  transition: '-webkit-transform 1s cubic-bezier(0.485, 1.65, 0.545, 0.835)'
-}
-
+  backgroundColor: "var(--secondaryColor)",
+  display: "none",
+  position: "absolute",
+  willChange: "transform",
+  transition: "-webkit-transform 1s cubic-bezier(0.485, 1.65, 0.545, 0.835)"
+};
 
 export {
-  infoBoxStyle, greatPlaceStyle,
-  greatPlaceCircleStyle, greatPlaceCircleStyleHover,
-  greatPlaceStickStyle, greatPlaceStickStyleHover, greatPlaceStickStyleShadow,
-  K_CIRCLE_SIZE, K_STICK_SIZE}
+  infoBoxStyle,
+  greatPlaceStyle,
+  greatPlaceCircleStyle,
+  greatPlaceCircleStyleHover,
+  greatPlaceStickStyle,
+  greatPlaceStickStyleHover,
+  greatPlaceStickStyleShadow,
+  K_CIRCLE_SIZE,
+  K_STICK_SIZE
+};
