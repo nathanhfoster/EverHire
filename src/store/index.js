d29b8c58d3b535696ee41805cc4c9f6499c40292
Nathan <nateinthegame@gmail.com>
2018-11-27 20:40:35 -0800

    dont need

--- src/store/index.js
+++ src/store/index.js
@@ -3,22 +3,8 @@ import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const consoleMessages = store => next => action => {
-  let result;
-  console.groupCollapsed(`dispatching action => ${action.type}`);
-  console.log("current user", store.getState().currentUser);
-  result = next(action);
-  let { currentUser } = store.getState();
-
-  console.log(`
-     current user: ${currentUser}
-     `);
-  console.groupEnd();
-  return result;
-};
-
 export default (initialState = {}) => {
-  return composeWithDevTools(applyMiddleware(thunk, consoleMessages))(
+  return composeWithDevTools(applyMiddleware(thunk))(
     createStore
   )(appReducer, initialState);
 };
